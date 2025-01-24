7aa05fcf329a43c6b3bc7190ed764f89dece0960
Amy Zhou <amyzhounyc@gmail.com>
2025-01-24 04:59:58 -0500

    added limits of HP setting

--- Adventurer.java
+++ Adventurer.java
@@ -111,7 +111,9 @@ public abstract class Adventurer{
 
   //Set Methods
   public void setHP(int health){
-    this.HP = health;
+    if (health <= getmaxHP()) {
+      this.HP = health;
+    }
   }
 
   public void setName(String s){
